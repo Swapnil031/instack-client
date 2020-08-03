3eb62f4926481c29e511715293ebb8915114f331
Swapnil031 <swapnilaudichya@gmail.com>
2020-08-03 17:41:08 +0530

    Updates

---
+++ precache-manifest.9481b3352380cef5fb5688867bb73f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bbecaa3f44500380e9e14885d42b40",
+    "url": "/instack-client/index.html"
+  },
+  {
+    "revision": "2fba6fcd4ccfbd09dcbd",
+    "url": "/instack-client/static/css/main.85b9d34a.chunk.css"
+  },
+  {
+    "revision": "296e9842cc7a58494427",
+    "url": "/instack-client/static/js/2.98616fe6.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/instack-client/static/js/2.98616fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fba6fcd4ccfbd09dcbd",
+    "url": "/instack-client/static/js/main.a76e7f9a.chunk.js"
+  },
+  {
+    "revision": "f3a135250d5fa43b360e",
+    "url": "/instack-client/static/js/runtime-main.d5f834f7.js"
+  }
+]);
